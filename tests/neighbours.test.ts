ad480a95e468269de945255e8be3787952c150f8
niklasgolf <niklas.herrloff@gmail.com>
2026-09-19 19:16:02 +0200

    Test orientation-based neighbour calculation

--- tests/neighbours.test.ts
+++ tests/neighbours.test.ts
@@ -1,7 +1,8 @@
 import { describe, expect, it } from 'vitest'
 import {
   getXDominatedNeighbours,
-  getYDominatedNeighbours
+  getYDominatedNeighbours,
+  getNeighbours
 } from '../src/neighbours.js'
 
 describe('getXDominatedNeighbours', () => {
@@ -54,6 +55,50 @@ describe('getYDominatedNeighbours', () => {
     }
 
     const neighbours = getYDominatedNeighbours(coordinate)
+
+    expect(neighbours).toEqual([
+      { x: 10, y: 8 },
+      { x: 10, y: 12 },
+      { x: 9, y: 9 },
+      { x: 11, y: 9 },
+      { x: 9, y: 11 },
+      { x: 11, y: 11 }
+    ])
+  })
+})
+
+describe('getNeighbours', () => {
+  it('uses x-dominated neighbour calculation', () => {
+    const coordinate = {
+      x: 10,
+      y: 10
+    }
+
+    const neighbours = getNeighbours(
+      coordinate,
+      'x-dominated'
+    )
+
+    expect(neighbours).toEqual([
+      { x: 8, y: 10 },
+      { x: 12, y: 10 },
+      { x: 9, y: 9 },
+      { x: 11, y: 9 },
+      { x: 9, y: 11 },
+      { x: 11, y: 11 }
+    ])
+  })
+
+  it('uses y-dominated neighbour calculation', () => {
+    const coordinate = {
+      x: 10,
+      y: 10
+    }
+
+    const neighbours = getNeighbours(
+      coordinate,
+      'y-dominated'
+    )
 
     expect(neighbours).toEqual([
       { x: 10, y: 8 },
