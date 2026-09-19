8a4218ded0299d946bc8757cf4ffb06c112f6b8a
niklasgolf <niklas.herrloff@gmail.com>
2026-09-19 11:14:59 +0200

    Expand coordinate validation tests

--- tests/coordinate.test.ts
+++ tests/coordinate.test.ts
@@ -12,4 +12,48 @@ describe('isValidCoordinate', () => {
 
     expect(result).toBe(true)
   })
+
+  it('returns true when x and y are both odd', () => {
+    const coordinate = {
+      x: 11,
+      y: 11
+    }
+
+    const result = isValidCoordinate(coordinate)
+
+    expect(result).toBe(true)
+  })
+
+  it('returns false when x is even and y is odd', () => {
+    const coordinate = {
+      x: 10,
+      y: 11
+    }
+
+    const result = isValidCoordinate(coordinate)
+
+    expect(result).toBe(false)
+  })
+
+  it('returns false when x is odd and y is even', () => {
+    const coordinate = {
+      x: 11,
+      y: 12
+    }
+
+    const result = isValidCoordinate(coordinate)
+
+    expect(result).toBe(false)
+  })
+
+  it('returns false when coordinates contain decimals', () => {
+    const coordinate = {
+      x: 10.5,
+      y: 10.5
+    }
+
+    const result = isValidCoordinate(coordinate)
+
+    expect(result).toBe(false)
+  })
 })
