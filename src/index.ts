d592a2bd1df43b9262e05b07a6d9b4677b857447
niklasgolf <niklas.herrloff@gmail.com>
2026-09-19 09:34:22 +0200

    Add grid orientation type

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export const MIDGARD_HEX_GRID_VERSION = '1.0.0'
 
 export type { Coordinate } from './coordinate.js'
 export { isValidCoordinate } from './coordinate.js'
+
+export type { GridOrientation } from './orientation.js'
---
+++ src/orientation.ts
@@ -0,0 +1,7 @@
+/**
+ * Represents the orientation of a Midgard hex grid.
+ *
+ * An x-dominated grid has horizontal straight movement.
+ * A y-dominated grid has vertical straight movement.
+ */
+export type GridOrientation = 'x-dominated' | 'y-dominated'
